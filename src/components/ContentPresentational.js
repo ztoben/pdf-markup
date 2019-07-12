6a563d96b295781d3706cf3c388c09d3a7fcbc75
Zach Toben <zach.toben@gmail.com>
2019-07-12 11:09:45 -0500

    add dropshadow to pdf canvases

--- src/components/ContentPresentational.js
+++ src/components/ContentPresentational.js
@@ -42,7 +42,8 @@ export default function ContentPresentational({
       display: 'flex',
       justifyContent: 'center',
       '& .pdf-canvas': {
-        display: 'none'
+        display: 'none',
+        boxShadow: '0px 1px 3px 0px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 2px 1px -1px rgba(0,0,0,0.12)'
       },
       [`& .canvas-${currentPage}`]: {
         display: 'flex'
